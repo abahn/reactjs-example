33c7816493f000ddd937f1b26148704084b0f7e0
badmanski <stanislav.gorski@gmail.com>
2017-03-31 19:16:53 +0300

    Add task class depending on its completion state

--- src/Task.js
+++ src/Task.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
 class Task extends Component {
+  stateClass() {
+    return this.props.task.completed
+      ? 'panel panel-success'
+      : 'panel panel-default';
+  }
+
   render() {
+    const stateClass = this.stateClass();
     return (
-      <div className="panel panel-default">
+      <div className={stateClass}>
         <div className="panel-heading"></div>
         <div className="panel-body">
           {this.props.task.description}
